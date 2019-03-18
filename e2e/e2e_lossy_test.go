715af862e2a580306f722017556bd1ddf005b187
Sean DuBois <sean@siobud.com>
2019-03-17 20:51:16 -0700

    Make inbound message handling stateless
    Instead of handling messages as they arrive we handle them in batches,
    giving us better error handling and insight into what state we are in.
    
    Relates to #43

--- e2e/e2e_lossy_test.go
+++ e2e/e2e_lossy_test.go
@@ -1,6 +1,7 @@
 package e2e
 
 import (
+	"math/rand"
 	"testing"
 	"time"
 
@@ -20,40 +21,49 @@ func TestPionE2ELossy(t *testing.T) {
 	}
 
 	for _, test := range []struct {
-		LossChance int
+		LossChanceRange int
 	}{
 		{
-			LossChance: 0,
+			LossChanceRange: 0,
 		},
 		{
-			LossChance: 10,
+			LossChanceRange: 10,
 		},
 		{
-			LossChance: 25,
+			LossChanceRange: 20,
 		},
 		{
-			LossChance: 50,
-		},
-		{
-			LossChance: 75,
+			LossChanceRange: 50,
 		},
 	} {
-		serverDone := make(chan interface{})
-		clientDone := make(chan interface{})
+		rand.Seed(time.Now().UTC().UnixNano())
+		chosenLoss := rand.Intn(9) + test.LossChanceRange
+		serverDone := make(chan error)
+		clientDone := make(chan error)
 		br := transportTest.NewBridge()
-		br.SetLossChance(test.LossChance)
+		if err := br.SetLossChance(chosenLoss); err != nil {
+			t.Fatal(err)
+		}
 
 		go func() {
-			dtls.Client(br.GetConn0(), &dtls.Config{})
-			close(clientDone)
+			if _, err := dtls.Client(br.GetConn0(), &dtls.Config{FlightInterval: time.Millisecond * 100}); err != nil {
+				clientDone <- err
+			} else {
+				close(clientDone)
+			}
 		}()
 
 		go func() {
-			dtls.Server(br.GetConn1(), &dtls.Config{
-				Certificate: serverCert,
-				PrivateKey:  serverKey,
-			})
-			close(serverDone)
+			if _, err := dtls.Server(br.GetConn1(), &dtls.Config{
+				Certificate:    serverCert,
+				PrivateKey:     serverKey,
+				FlightInterval: time.Millisecond * 100,
+			}); err != nil {
+				serverDone <- err
+			} else {
+				close(serverDone)
+
+			}
 		}()
 
 		testTimer := time.NewTimer(lossyTestTimeout)
@@ -63,14 +73,22 @@ func TestPionE2ELossy(t *testing.T) {
 				break
 			}
 
-			br.Process()
+			br.Tick()
 			select {
-			case <-serverDone:
+			case err, ok := <-serverDone:
+				if ok {
+					t.Fatalf("Fail, serverError: clientComplete(%t) serverComplete(%t) LossChance(%d) error(%v)", clientComplete, serverComplete, chosenLoss, err)
+				}
+
 				serverComplete = true
-			case <-clientDone:
+			case err, ok := <-clientDone:
+				if ok {
+					t.Fatalf("Fail, clientError: clientComplete(%t) serverComplete(%t) LossChance(%d) error(%v)", clientComplete, serverComplete, chosenLoss, err)
+				}
+
 				clientComplete = true
 			case <-testTimer.C:
-				t.Fatalf("Test expired: clientComplete(%t) serverComplete(%t) LossChance(%d)", clientComplete, serverComplete, test.LossChance)
+				t.Fatalf("Test expired: clientComplete(%t) serverComplete(%t) LossChance(%d)", clientComplete, serverComplete, chosenLoss)
 			default:
 			}
 		}
--- handshake_cache.go
+++ handshake_cache.go
@@ -1,56 +1,70 @@
 package dtls
 
 type handshakeCacheItem struct {
-	flight                 flightVal
-	isLocal                bool
-	epoch, messageSequence uint16
-	data                   []byte
+	typ             handshakeType
+	isClient        bool
+	messageSequence uint16
+	data            []byte
+}
+
+type handshakeCachePullRule struct {
+	typ      handshakeType
+	isClient bool
 }
 
 type handshakeCache struct {
-	cache []handshakeCacheItem
+	cache []*handshakeCacheItem
 }
 
 func newHandshakeCache() *handshakeCache {
 	return &handshakeCache{}
 }
 
-func (h *handshakeCache) push(data []byte, epoch, messageSequence uint16, isLocal bool, currentFlight flightVal) {
+func (h *handshakeCache) push(data []byte, messageSequence uint16, typ handshakeType, isClient bool) {
 	for _, i := range h.cache {
-		if i.isLocal == isLocal &&
-			i.epoch == epoch &&
-			i.messageSequence == messageSequence {
+		if i.messageSequence == messageSequence &&
+			i.isClient == isClient {
 			return
 		}
 	}
-	h.cache = append(h.cache, handshakeCacheItem{
-		flight:          currentFlight,
+
+	h.cache = append(h.cache, &handshakeCacheItem{
 		data:            append([]byte{}, data...),
-		epoch:           epoch,
 		messageSequence: messageSequence,
-		isLocal:         isLocal,
+		typ:             typ,
+		isClient:        isClient,
 	})
 }
 
-type handshakeCacheExcludeRule struct {
-	isLocal  bool // Exclude handshake if we sent
-	isRemote bool // Exclude handshake if remote sent
+// returns a list handshakes that match the requested rules
+// the list will contain null entries for rules that can't be satisfied
+// multiple entries may match a rule, but only the last match is returned (ie ClientHello with cookies)
+func (h *handshakeCache) pull(rules ...handshakeCachePullRule) []*handshakeCacheItem {
+	out := make([]*handshakeCacheItem, len(rules))
+	for i, r := range rules {
+		for _, c := range h.cache {
+			if c.typ == r.typ && c.isClient == r.isClient {
+				switch {
+				case out[i] == nil:
+					out[i] = c
+				case out[i].messageSequence < c.messageSequence:
+					out[i] = c
+				}
+			}
+		}
+	}
+
+	return out
 }
 
-func (h *handshakeCache) combinedHandshake(excludeRules map[flightVal]handshakeCacheExcludeRule, excludeLast bool) []byte {
-	out := make([]byte, 0)
-	lastIndex := len(h.cache) - 1 // Safe if len(h.cache) == 0, no loop will occur
-	for i, v := range h.cache {
-		if e, ok := excludeRules[v.flight]; ok {
-			if e.isLocal && v.isLocal {
-				continue
-			} else if e.isRemote && !v.isLocal {
-				continue
-			}
-		} else if excludeLast && i == lastIndex {
-			break
+// pullAndMerge calls pull and then merges the results, ignoring any null entries
+func (h *handshakeCache) pullAndMerge(rules ...handshakeCachePullRule) []byte {
+	merged := []byte{}
+
+	for _, p := range h.pull(rules...) {
+		if p != nil {
+			merged = append(merged, p.data...)
 		}
-		out = append(out, v.data...)
 	}
-	return out
+	return merged
 }
